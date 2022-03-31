14b1f21f293806fd39a972fa74f0eec64fdc998f
Morten Just <morten.just@genstart.dk>
2022-03-31 12:41:42 +0200

    Initial Commit

---
+++ VideoRotation/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VideoRotation
+//
+//  Created by Morten Just on 3/31/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
